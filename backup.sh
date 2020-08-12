82c3c36fc05e5eaa583063159359942889a8a17d
Azat Bahawi <azahi@teknik.io>
2020-08-12 15:23:29 +0300

    pgsql is being a pain

--- backup.sh
+++ backup.sh
@@ -6,11 +6,8 @@ BACKUPDIR="$PWD"
     mkdir --parents --verbose "$BACKUPDIR/etc/portage"
 [ ! -d "$BACKUPDIR/var/lib/portage" ] && \
     mkdir --parents --verbose "$BACKUPDIR/var/lib/portage"
-[ ! -d "$BACKUPDIR/var/db/repos/local" ] && \
-    mkdir --parents --verbose "$BACKUPDIR/var/db/repos/local"
 
 cp --recursive --verbose /etc/portage/* "$BACKUPDIR/etc/portage/"
 cp --recursive --verbose /var/lib/portage/world "$BACKUPDIR/var/lib/portage/"
-cp --recursive --verbose /var/db/repos/local/* "$BACKUPDIR/var/db/repos/local"
 
 zcat /proc/config.gz > .config
