a605fb0d062566c7deea432367b503d9967c266a
Azat Bahawi <azahi@teknik.io>
2020-09-20 23:37:12 +0300

    Handled a particular error.

--- backup.sh
+++ backup.sh
@@ -1,13 +1,97 @@
 #!/bin/sh
 
-BACKUPDIR="$PWD"
+### Functions
+prepare()
+{
+    for i in etc var usr
+    do
+        [ -d "./$i" ] && \
+            rm -rf "./$i"
+    done
+}
 
-[ ! -d "$BACKUPDIR/etc/portage" ] && \
-    mkdir --parents --verbose "$BACKUPDIR/etc/portage"
-[ ! -d "$BACKUPDIR/var/lib/portage" ] && \
-    mkdir --parents --verbose "$BACKUPDIR/var/lib/portage"
+mkdir_check()
+{
+    [ ! -d "$1" ] && \
+        mkdir -p "$1"
+}
 
-cp --recursive --verbose /etc/portage/* "$BACKUPDIR/etc/portage/"
-cp --recursive --verbose /var/lib/portage/world "$BACKUPDIR/var/lib/portage/"
+rm_check()
+{
+    if [ -d "$1" ] || [ -f "$1" ]
+    then
+        rm -rf "$1"
+    fi
+}
 
-zcat /proc/config.gz > .config
+dump_dir()
+{
+    mkdir_check ".$1"
+    rsync --archive "$1/" ".$1"
+}
+
+dump_file()
+{
+    rsync --archive "$1" ".$1"
+}
+
+### Prepare
+prepare
+
+### Dump
+# portage
+dump_dir /etc/portage
+mkdir_check ./var/lib/portage
+dump_file /var/lib/portage/world
+dump_file /var/lib/portage/world_sets
+rm_check ./etc/portage/bashrc
+rm_check ./etc/portage/bashrc.d
+rm_check ./etc/portage/make.conf.lto
+rm_check ./etc/portage/make.conf.lto.defines
+
+# X11
+dump_dir /etc/X11/xorg.conf.d
+
+# Kernel
+mkdir_check ./usr/src/linux
+zcat /proc/config.gz > ./usr/src/linux/.config
+
+# Kernel modules
+dump_dir /etc/modprobe.d
+dump_dir /etc/modules-load.d
+
+# sysctl
+#dump_dir /etc/sysctl.d
+#dump_file /etc/sysctl.conf
+
+# env.d
+dump_dir /etc/env.d
+
+# fontconfig
+dump_dir /etc/fonts/conf.d
+
+# LMT
+mkdir_check ./etc/laptop-mode
+dump_file /etc/laptop-mode/laptop-mode.conf
+mkdir_check ./etc/laptop-mode/conf.d
+dump_file /etc/laptop-mode/conf.d/intel-hda-powersave.conf
+dump_file /etc/laptop-mode/conf.d/intel-sata-powermgmt.conf
+dump_dir /etc/pm
+
+# syslog-ng
+mkdir_check ./etc/syslog-ng
+dump_file /etc/syslog-ng/syslog-ng.conf
+
+# logrotate
+dump_dir /etc/logrotate.d
+dump_file /etc/logrotate.conf
+
+# Misc.
+dump_file /etc/buildkernel.conf
+dump_file /etc/dispatch-conf.conf
+dump_file /etc/fstab
+dump_file /etc/genkernel.conf
+dump_file /etc/hdparm.conf
+dump_file /etc/issue
+dump_file /etc/locale.gen
+dump_file /etc/thinkfan.conf
